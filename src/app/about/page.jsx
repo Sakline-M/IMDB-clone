65ca14f2c48c96f3194863a298931a847b3a44bf
SM Sakin <saklinemostaksakin58@example.com>
2024-10-04 02:19:35 +0600

    half done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="max-w-6xl m-auto p-3 space-y-4">
+        
+        <h1 className="text-2xl font-medium text-amber-500">About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur modi, corrupti perspiciatis facilis provident maxime adipisci voluptas est ad exercitationem architecto facere doloribus magnam ex repellat tempora voluptatibus. Officia voluptas maiores quibusdam debitis. ?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur modi, corrupti perspiciatis facilis provident maxime adipisci voluptas est ad exercitationem architecto facere doloribus magnam ex repellat tempora voluptatibus. Officia voluptas maiores quibusdam debitis. Quaerat assumenda consectetur illum similique corporis error? </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur modi, corrupti perspiciatis facilis provident maxime adipisci voluptas est ad exercitationem architecto facere doloribus magnam ex repellat tempora voluptatibus. Officia voluptas maiores quibusdam debitis. Quaerat assumenda consectetur illum similique corporis error? Voluptas, ducimus. Eum vitae iure eligendi beatae repudiandae tempora obcaecati? Voluptas, ducimus. Eum vitae iure eligendi beatae repudiandae tempora obcaecati? Voluptas, ducimus. Eum vitae iure eligendi beatae repudiandae tempora obcaecati? Voluptas, ducimus. Eum vitae iure eligendi beatae repudiandae tempora obcaecati?Voluptas, ducimus. Eum vitae iure eligendi beatae repudiandae tempora obcaecati?</p>
+    </div>
+  )
+}
